42695e632ca98118a1c98a4836971d4212db7908
rokussadee <rokus.sadee@student.ehb.be>
2023-12-12 17:18:38 +0100

    feat(logo-collection): collection & logo component
    src/components/collection:
    Collection component & scss
    src/components/logo:
    - Logo component: load in svg paths and animate on hoveri using Framer
      Motion, dynamic viewBox on window resize
    - scss styling

--- animated-portfolio-starter/src/components/logo/Logo.jsx
+++ animated-portfolio-starter/src/components/logo/Logo.jsx
@@ -1,12 +1,15 @@
 import {motion} from "framer-motion";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
+import "./logo.scss";
 
 const Logo = () =>{
 
-   const [paths, setPaths] = useState({
+  const [paths, setPaths] = useState({
     logo: { d: "" },
-    logo_2: { d: "" },
-  });
+    logo_3: { d: "" },
+  });  
+
+  const svgRef = useRef(null);
 
   useEffect(() => {
     const fetchPaths = async () => {
@@ -22,29 +25,71 @@ const Logo = () =>{
     fetchPaths();
   }, []);
 
+  useEffect(() => {
+    const updateViewBox = () => {
+      const boundingBox = svgRef.current.getBBox();
+      const padding = 5; // Add padding if needed
+      const newViewBox = `${boundingBox.x - padding} ${boundingBox.y - padding} ${boundingBox.width + padding * 2} ${boundingBox.height + padding * 2}`;
+      svgRef.current.setAttribute("viewBox", newViewBox);
+    };
+    
+    const initialSetDAttribute = () => {
+      // Set the initial "d" attribute value
+      svgRef.current.querySelector("path").setAttribute("d", paths.logo.d);
+    };
+
+    updateViewBox();
+    
+    initialSetDAttribute();
+    
+    // Update viewBox whenever paths change
+    window.addEventListener("resize", updateViewBox);
+
+    return () => {
+      window.removeEventListener("resize", updateViewBox);
+    };
+  }, [paths]);
+
   const variants = {
     default: {
-      d: paths.logo.d
+      d: paths.logo_3.d
     },
  
     hover: {
-      d: paths.logo_2.d
+      d: paths.logo.d
     },
   };
 
   return (
     <div>
-          <motion.div 
-        className="bg"
+      <motion.div 
+        className="logo-wrapper"
         whileHover="hover"
         initial="default"
         animate="default">
-        <motion.svg viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg">
+        <motion.svg 
+          ref={svgRef}
+          className="logo-svg" 
+          xmlns="http://www.w3.org/2000/svg">
           <motion.path 
+            className="logo-path"
             variants={variants}
-            strokeWidth="1"
+            strokeWidth=".1"
             fill="none"
-            stroke="grey">
+            stroke="white"
+            initial={{pathLength: 0}}
+            whileInView={{pathLength:1}}
+          >
+          </motion.path>
+          <motion.path
+            className="blur-path"
+            variants={variants}
+            strokeWidth=".1"
+            fill="none"
+            stroke="red"
+            initial={{pathLength: 0}}
+            whileInView={{pathLength:1}}
+          >
           </motion.path>
         </motion.svg>
       </motion.div>
